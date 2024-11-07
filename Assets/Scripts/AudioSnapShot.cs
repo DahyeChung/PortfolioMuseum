c1438aaa818352761eb95fcdca9d9aa43048f8c1
DahyeChung <lewachung@gmail.com>
2024-11-06 20:55:47 -0500

    AudioMixer test

---
+++ Assets/Scripts/AudioSnapShot.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSnapShot : MonoBehaviour
+{
+    public AudioMixer MainAudioMixer;
+    public AudioMixerSnapshot NormalSnapShot;
+    public AudioMixerSnapshot UnderwaterSnapShot;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            ApplyAudioSnapshot(UnderwaterSnapShot, 1.0f);
+        }
+    }
+
+    public void ApplyAudioSnapshot(AudioMixerSnapshot snapshot, float translationTime)
+    {
+        snapshot.TransitionTo(translationTime);
+    }
+}
