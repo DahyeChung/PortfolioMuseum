9f4057589014bc8fd2f23b4cb1bea4f220992fa7
DahyeChung <lewachung@gmail.com>
2024-12-08 18:58:50 -0500

    Door to the outside

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +6,42 @@ public class Door : MonoBehaviour
     public string sceneName;
     public Material deemphasizedMat;
     public Material emphasizedMat;
+    public GameObject player;
 
     private MeshRenderer meshRenderer;
+
+
 
     void Start()
     {
         meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(Vector3.Distance(transform.position, player.transform.position));
+
+        // compare tag and if the player is close to the door, emphasize it
+        if (Vector3.Distance(transform.position, player.transform.position) < 25)
+        {
+            Debug.Log("문 감지 머티리얼 변경");
+            Emphasize();
+        }
+        else
+        {
+            Deemphasize();
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Debug.Log("트리거 발동");
+            Open();
+        }
     }
 
     public void Emphasize()
