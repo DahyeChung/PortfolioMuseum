5c9088813fe11895311deec829ee17ae94f5335b
Dahye Chung <lewachung@gmail.com>
2024-12-10 23:14:18 -0500

    Garden Design Progress

--- Assets/Scripts/Model3DDisplay.cs
+++ Assets/Scripts/Model3DDisplay.cs
@@ -6,6 +6,7 @@ public class Model3DDisplay : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        // Rotate the object around its local Y axis at 50 degrees per second
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
 }
